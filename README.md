1b3fa102c1f8a253f49e3d01872e399e52c9cdc4
Keenan Tullis <keenanjt@umich.edu>
2021-05-13 16:22:00 -0400

    Adds basic accuweather api call

--- README.md
+++ README.md
@@ -14,3 +14,15 @@ Clone this repository: `git clone https://github.com/keenanjt33/weather-or-not.g
   [these instructions](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
 
 Install dependencies: `npm install`
+
+Set up an AccuWeather [developer account](https://developer.accuweather.com/)
+then register an [app](https://developer.accuweather.com/user/me/apps)
+
+Create a file called `.env` in the repository's root directory and add the line:
+
+```
+WEATHER_KEY='<accuweather_app_api_key>'
+```
+
+Where `<accuweather_app_api_key>` is the API key registered to your AccuWeather
+app
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const express = require('express')
+const express = require('express');
+const fetch = require('node-fetch');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+require('dotenv').config();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.get('/api', (req, res) => {
+  fetch(
+    `https://dataservice.accuweather.com/locations/v1/search?q=chicago&apikey=${process.env.WEATHER_KEY}`
+  )
+    .then((apiRes) => apiRes.json())
+    .then((text) => res.send(text));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`weather-or-not listening at http://localhost:${port}`);
+});
